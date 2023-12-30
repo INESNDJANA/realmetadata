f04bc043b775640ca7f0af70fee33c2db5ab6a44
INESNDJANA <94466445+INESNDJANA@users.noreply.github.com>
2023-12-30 20:25:22 +0100

    Add files via upload

--- EXTRACTMETADATAREELLE/app.py
+++ EXTRACTMETADATAREELLE/app.py
@@ -1,14 +1,23 @@
 
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, Response, make_response
 import os
-from PyPDF2 import PdfFileReader
 
+from PyPDF2 import PdfFileReader
 from docx import Document
 from openpyxl import load_workbook
 from pptx import Presentation
 from PIL import Image
 from moviepy.editor import VideoFileClip
 from werkzeug.utils import secure_filename
+from mutagen.mp3 import MP3
+from mutagen.easyid3 import EasyID3
+from mutagen.flac import FLAC
+from mutagen.wavpack import WavPack
+from mutagen.aiff import AIFF
+from moviepy.editor import VideoFileClip
+from datetime import datetime
+from PyPDF2.generic import IndirectObject
+from io import StringIO
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -21,30 +30,22 @@ def index():
 def upload():
     if 'folder' in request.files:
         uploaded_files = request.files.getlist('folder')
-
-        # Assurez-vous que le dossier temporaire existe
         temp_folder = UPLOAD_FOLDER
         os.makedirs(temp_folder, exist_ok=True)
-
-        # Enregistrez chaque fichier dans le dossier temporaire
         for uploaded_file in uploaded_files:
             filename = secure_filename(uploaded_file.filename)
             file_path = os.path.join(temp_folder, filename)
             uploaded_file.save(file_path)
 
-        # Nettoyez la liste des métadonnées avant d'ajouter de nouvelles données
         all_file_metadata.clear()
-
-        # Parcourez tous les fichiers du dossier temporaire et extrayez les métadonnées
         for root, dirs, files in os.walk(temp_folder):
             for file_name in files:
                 file_path = os.path.join(root, file_name)
                 metadata = extract_metadata(file_path)
                 if metadata:
-                    # Ajoutez les métadonnées à la liste
+                   
                     all_file_metadata.append({'file_name': file_name, 'metadata': metadata})
 
-        # Redirigez vers la page de métadonnées
         return redirect(url_for('metadata', folder_path=temp_folder))
     else:
         return render_template('index.html', error="No folder selected")
@@ -61,89 +62,236 @@ def simple_convert(metadata):
     else:
         return str(metadata)
 
+def simple_convert(metadata):
+    if isinstance(metadata, dict):
+        return {k: convert_value(v) for k, v in metadata.items()}
+    elif isinstance(metadata, list):
+        return [convert_value(item) for item in metadata]
+    else:
+        return convert_value(metadata)
+
+def convert_value(value):
+    if isinstance(value, datetime):
+        return value.strftime('%Y-%m-%d %H:%M:%S')
+    elif isinstance(value, IndirectObject):
+        
+        return str(value)
+    elif isinstance(value, (list, dict)):
+        return simple_convert(value)
+    elif value is None:
+        return 'None'
+    else:
+        return value
 @app.route('/metadata_data')
 def metadata_data():
     for item in all_file_metadata:
-        print(item)  # Log pour inspecter les données
-   
+        print(item)  
     return jsonify(simple_convert(all_file_metadata))
+def convert_to_text(metadata):
+    lines = []
+    for item in metadata:
+        lines.append(f"File Name: {item['file_name']}")
+        for key, value in item['metadata'].items():
+            lines.append(f"{key}: {value}")
+        lines.append("\n")  
+    return "\n".join(lines)
 
+import csv
+from flask import Response
 
+@app.route('/download_metadata')
+def download_metadata():
+    si = StringIO()
+    cw = csv.writer(si)
+    
+    # En-têtes des colonnes
+    cw.writerow(['File Name', 'Attribute', 'Value'])
+
+    for file_data in all_file_metadata:
+        for key, value in file_data['metadata'].items():
+            cw.writerow([file_data['file_name'], key, value])
+
+    output = make_response(si.getvalue())
+    output.headers["Content-Disposition"] = "attachment; filename=metadata.csv"
+    output.headers["Content-type"] = "text/csv"
+    return output
 
 def extract_docx_metadata(file_path):
-    doc = Document(file_path)
-    docx_metadata = {
-        'Title': doc.core_properties.title,
-        'Author': doc.core_properties.author,
-        'Created': doc.core_properties.created,
-        'Modified': doc.core_properties.modified,
-    }
-    return docx_metadata
+    try:
+        doc = Document(file_path)
+        return {
+            'Title': doc.core_properties.title or "Inconnu",
+            'Author': doc.core_properties.author or "Inconnu",
+            'Created': doc.core_properties.created or "Inconnu",
+            'Modified': doc.core_properties.modified or "Inconnu",
+            'Subject': doc.core_properties.subject or "Inconnu",
+            'Comments': doc.core_properties.comments or "Inconnu",
+            'Category': doc.core_properties.category or "Inconnu",
+            'Keywords': doc.core_properties.keywords or "Inconnu",
+            'Last Modified By': doc.core_properties.last_modified_by or "Inconnu",
+            'Revision': doc.core_properties.revision or "Inconnu"
+            
+        }
+    except Exception as e:
+        return {"Error": str(e)}
 
+
+
+from openpyxl import load_workbook
 
 def extract_xlsx_metadata(file_path):
-    excel_doc = load_workbook(file_path)
-    properties = excel_doc.properties
-    xlsx_metadata = {
-        'Title': properties.title,
-        'Author': properties.author,
-        'Created': properties.created,
-        'Modified': properties.modified,
-    }
-    return xlsx_metadata
+    try:
+        excel_doc = load_workbook(file_path)
+        properties = excel_doc.properties
+        return {
+            'Title': properties.title or "Inconnu",
+            'Author': properties.author or "Inconnu",
+            'Created': properties.created or "Inconnu",
+            'Modified': properties.modified or "Inconnu",
+            'Last Modified By': properties.last_modified_by or "Inconnu",
+            'Category': properties.category or "Inconnu",
+            'Keywords': properties.keywords or "Inconnu",
+            'Comments': properties.comments or "Inconnu",
+            'Subject': properties.subject or "Inconnu"
+            
+        }
+    except Exception as e:
+        return {"Error": str(e)}
+
 
 def extract_pptx_metadata(file_path):
-    pptx_doc = Presentation(file_path)
-    pptx_metadata = {
-        'Title': pptx_doc.core_properties.title,
-        'Author': pptx_doc.core_properties.author,
-        'Created': pptx_doc.core_properties.created,
-        'Modified': pptx_doc.core_properties.modified,
-    }
-    return pptx_metadata
+    try:
+        pptx_doc = Presentation(file_path)
+        return {
+            'Title': pptx_doc.core_properties.title or "Inconnu",
+            'Author': pptx_doc.core_properties.author or "Inconnu",
+            'Created': pptx_doc.core_properties.created or "Inconnu",
+            'Modified': pptx_doc.core_properties.modified or "Inconnu",
+            'Last Modified By': pptx_doc.core_properties.last_modified_by or "Inconnu",
+            'Revision': pptx_doc.core_properties.revision or "Inconnu",
+            'Subject': pptx_doc.core_properties.subject or "Inconnu",
+            'Keywords': pptx_doc.core_properties.keywords or "Inconnu",
+            'Comments': pptx_doc.core_properties.comments or "Inconnu",
+            'Category': pptx_doc.core_properties.category or "Inconnu"
+          
+        }
+    except Exception as e:
+        return {"Error": str(e)}
+
 def extract_pdf_metadata(pdf_path):
     with open(pdf_path, 'rb') as pdf_file:
         pdf_reader = PdfFileReader(pdf_file)
         info = pdf_reader.getDocumentInfo()
         return simple_convert(info)        
+from PIL import Image
+from PIL.ExifTags import TAGS, GPSTAGS
+
+def get_decimal_from_dms(dms, ref):
+    degrees, minutes, seconds = dms
+    decimal = degrees + (minutes / 60.0) + (seconds / 3600.0)
+    if ref in ['S', 'W']:
+        decimal = -decimal
+    return decimal
+
+def get_gps_coordinates(exif_data):
+    if 'GPSInfo' in exif_data:
+        gps_info = exif_data['GPSInfo']
+        gps_latitude = get_decimal_from_dms(gps_info[2], gps_info[1])
+        gps_longitude = get_decimal_from_dms(gps_info[4], gps_info[3])
+        return gps_latitude, gps_longitude
+    return None, None
+
 def extract_image_metadata(image_path):
     with Image.open(image_path) as img:
+        exif_data = {}
+        if hasattr(img, '_getexif'):
+            exif_info = img._getexif()
+            if exif_info is not None:
+                for tag, value in exif_info.items():
+                    decoded = TAGS.get(tag, tag)
+                    exif_data[decoded] = value
+
+        gps_latitude, gps_longitude = get_gps_coordinates(exif_data)
+
         image_metadata = {
             'Format': img.format,
             'Mode': img.mode,
             'Size': img.size,
             'Info': img.info,
-            # Ajoutez d'autres métadonnées en fonction de vos besoins
+            'GPS Latitude': gps_latitude,
+            'GPS Longitude': gps_longitude,
         }
-    return image_metadata
+        return image_metadata
+
+    return {"Error": "Unable to open image or read EXIF data"}
+def extract_audio_metadata(audio_path):
+    file_extension = audio_path.split('.')[-1].lower()
+    metadata = {}
+
+    try:
+        if file_extension == "mp3":
+            audio = MP3(audio_path, ID3=EasyID3)
+        elif file_extension == "flac":
+            audio = FLAC(audio_path)
+        elif file_extension == "wv":
+            audio = WavPack(audio_path)
+        elif file_extension == "aiff":
+            audio = AIFF(audio_path)
+        else:
+            return {"Error": "Unsupported audio format"}
+
+        for key in audio.keys():
+            metadata[key] = audio[key]
+
+        return metadata
+    except Exception as e:
+        return {"Error": str(e)}
 
 
-#def extract_audio_metadata(audio_path):
-  #  audio = AudioSegment.from_file(audio_path)
-    
-    #audio_metadata = {
-        #'Channels': audio.channels,
-       # 'Sample Width': audio.sample_width,
-        #'Frame Rate': audio.frame_rate,
-       # 'Frame Width': audio.frame_width,
-        #'Length (ms)': len(audio),
-        #'Tags': audio.tags,  # Vous pouvez personnaliser cela en fonction de ce que vous voulez extraire
-   # }
-    
-    #return audio_metadata
-from moviepy.editor import VideoFileClip
+
+def format_duration(seconds):
+    hours = int(seconds // 3600)
+    minutes = int((seconds % 3600) // 60)
+    seconds = seconds % 60
+    return f"{hours}h {minutes}m {seconds:.2f}s"
+
 
 def extract_video_metadata(video_path):
-    clip = VideoFileClip(video_path)
-    
-    video_metadata = {
-        'Duration (s)': clip.duration,
-        'Size': clip.size,
-        'FPS': clip.fps,
-        # Ajoutez d'autres métadonnées en fonction de vos besoins
-    }
-    
+    with VideoFileClip(video_path) as clip:
+        width, height = clip.size
+        aspect_ratio = f"{width}:{height}"
+
+        video_metadata = {
+            'Duration (s)': clip.duration,
+            'Readable Duration': format_duration(clip.duration),
+            'Width': width,
+            'Height': height,
+            'Aspect Ratio': aspect_ratio,
+            'FPS': clip.fps,
+        }
+
+        if hasattr(clip.reader, 'bitrate'):
+            video_metadata['Bitrate (kbps)'] = clip.reader.bitrate / 1000
+        else:
+            video_metadata['Bitrate (kbps)'] = 'Unknown'
+
+        if clip.audio and hasattr(clip.audio, 'nchannels'):
+            video_metadata['Audio Channels'] = clip.audio.nchannels
+        else:
+            video_metadata['Audio Channels'] = 'None'
+
+        if hasattr(clip.reader, 'codec'):
+            video_metadata['Video Codec'] = clip.reader.codec
+        else:
+            video_metadata['Video Codec'] = 'Unknown'
+
+        if clip.audio and hasattr(clip.audio.reader, 'codec'):
+            video_metadata['Audio Codec'] = clip.audio.reader.codec
+        else:
+            video_metadata['Audio Codec'] = 'Unknown'
+
     return video_metadata
+
 
 def extract_metadata(file_path):
     file_extension = file_path.split('.')[-1].lower()
@@ -160,8 +308,8 @@ def extract_metadata(file_path):
             return extract_pdf_metadata(file_path)
         elif file_extension in ['jpg', 'jpeg', 'png', 'gif']:
             return extract_image_metadata(file_path)
-       # elif file_extension in ['mp3', 'wav']:
-        #    return extract_audio_metadata(file_path)
+        elif file_extension in ['mp3', 'wav']:
+            return extract_audio_metadata(file_path)
         elif file_extension in ['mp4', 'avi', 'mkv']:
             return extract_video_metadata(file_path)
     else:
